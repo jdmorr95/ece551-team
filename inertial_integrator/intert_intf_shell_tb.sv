033e4e320ec88f443d7371773f4166a509b4cdc9
Charlie Harrington <ctharrington@wisc.edu>
2024-11-06 10:39:53 -0600

    Merge Intertial Interface Shell Changes.

--- inertial_integrator/intert_intf_shell_tb.sv
+++ inertial_integrator/intert_intf_shell_tb.sv
@@ -1,30 +1,26 @@
-module inert_intf_shell_tb()
-
-
+module inert_intf_shell_tb #(
     parameter FAST_SIM = 1;
     parameter CLK_PERIOD = 10;
+) (
+    input clk, rst_n,
+    input strt_cal,						// goes high to initiate calibration
+    input vld,							// goes high for 1 clock cycle when new data valid
+    input signed [15:0] yaw_rt,			// raw gyro rate readings from inert_intf
+    input lftIR,rghtIR,					// gaurdrail signals (fusion with gyro)
+    input moving,						// Only integrate yaw when "going"
+    output logic cal_done,				// asserted when calibration is completed
+    output reg rdy,
+    output signed [11:0] heading,
+    output [7:0] LED
 
-    logic clk;
-    logic rst_n;
-    logic strt_cal;
-    logic cal_done;
-    logic signed [11:0] heading;
-    logic rdy;
-    logic lftIR;
-    logic rghtIR;
-    logic SS_n;
-    logic SCLK;
-    logic MOSI;
-    logic MISO;
-    logic INT;
-    logic moving;
-
+    
+);
 
     // instantiate inertial interface
-    inert_intf DUT( .clk(clk),.rst_n(rst_n),.strt_cal(strt_cal),.cal_done(cal_done),
-                    .heading(heading),.rdy(rdy),.lftIR(lftIR),.rghtIR(rghtIR),.SS_n(SS_n),
-                    .SCLK(SCLK),.MOSI(MOSI),.MISO(MISO),.INT(INT),.moving(moving)
-                    );
+    inert_intf DUT( .clk(clk),.rst_n(rst_n),.strt_cal(strt_cal),.vld(vld),.yaw_rt(yaw_rt),
+                    .lftIR(lftIR),.rghtIR(rghtIR),.moving(moving),.cal_done(cal_done),.rdy(rdy),
+                    .heading(heading),.LED(LED) );
+                    
 
     // instantiate iNEMO
     SPI_iNEMO2 SPI_iNEMO2(.SS_n(SS_n),.SCLK(SCLK),.MISO(MISO),.MOSI(MOSI),.INT(INT));
@@ -33,6 +29,9 @@ module inert_intf_shell_tb()
     // Testbench Code //
     ////////////////////
     initial begin
+        clk = 0;
+        rst_n = 0;
+
     end
 
 
