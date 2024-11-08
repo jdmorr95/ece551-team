ba0886f39241d8dc7a4f5387badf97363eb1c16f
jdmorr95 <104585560+jdmorr95@users.noreply.github.com>
2024-11-08 10:48:57 -0600

    Update Inert_Intf TB Port Instantiations.

--- inertial_integrator/intert_intf_shell_tb.sv
+++ inertial_integrator/intert_intf_shell_tb.sv
@@ -1,20 +1,23 @@
 module inert_intf_shell_tb #(
-    parameter FAST_SIM = 1;
-    parameter CLK_PERIOD = 10;
-) (
-    input clk, rst_n,
-    input strt_cal,						// goes high to initiate calibration
-    input vld,							// goes high for 1 clock cycle when new data valid
-    input signed [15:0] yaw_rt,			// raw gyro rate readings from inert_intf
-    input lftIR,rghtIR,					// gaurdrail signals (fusion with gyro)
-    input moving,						// Only integrate yaw when "going"
-    output logic cal_done,				// asserted when calibration is completed
-    output reg rdy,
-    output signed [11:0] heading,
-    output [7:0] LED
-
-    
+    parameter FAST_SIM = 1,
+    parameter CLK_PERIOD = 10
 );
+    logic clk;
+    logic rst_n;
+    logic strt_cal;					// goes high to initiate calibration
+    logic lftIR;
+    logic rghtIR;					// gaurdrail signals (fusion with gyro)
+    logic moving;						// Only integrate yaw when "going"
+    logic cal_done;				// asserted when calibration is completed
+    logic rdy;
+    logic signed [11:0] heading;
+    logic [7:0] LED;
+
+    logic SS_n;
+    logic SCLK;
+    logic MOSI;
+    logic MISO;
+    logic INT;
 
     // instantiate inertial interface
     inert_intf DUT( .clk(clk),.rst_n(rst_n),.strt_cal(strt_cal),.vld(vld),.yaw_rt(yaw_rt),
